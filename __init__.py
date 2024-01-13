cd47fa5bfc13bf55c83dd88c1ec8fa539f90a440
wailovet <wailovet@163.com>
2024-01-14 01:53:46 +0800

    'fix'

--- __init__.py
+++ __init__.py
@@ -23,15 +23,23 @@ from typing import List, Dict, Tuple
 from nodes import MAX_RESOLUTION, SaveImage
 import comfy.utils
 import torch.nn.functional as F
+import shutil
 
+translation_dir = os.path.join(os.path.dirname(
+    os.path.dirname(__file__)), "AIGODLIKE-COMFYUI-TRANSLATION")
 
+translation_config = os.path.join(os.path.dirname(__file__), "ComfyUI-WW.json")
+if os.path.exists(translation_dir):
+    shutil.copy(translation_config, translation_dir)
 
 
 def p(image):
-    return image.permute([0,3,1,2])
+    return image.permute([0, 3, 1, 2])
+
 
 def pb(image):
-    return image.permute([0,2,3,1])
+    return image.permute([0, 2, 3, 1])
+
 
 class ImageResize:
     @classmethod
@@ -39,10 +47,10 @@ class ImageResize:
         return {
             "required": {
                 "image": ("IMAGE",),
-                "width": ("INT", { "default": 512, "min": 0, "max": MAX_RESOLUTION, "step": 8, }),
-                "height": ("INT", { "default": 512, "min": 0, "max": MAX_RESOLUTION, "step": 8, }),
+                "width": ("INT", {"default": 512, "min": 0, "max": MAX_RESOLUTION, "step": 8, }),
+                "height": ("INT", {"default": 512, "min": 0, "max": MAX_RESOLUTION, "step": 8, }),
                 "interpolation": (["nearest", "bilinear", "bicubic", "area", "nearest-exact", "lanczos"],),
-                "keep_proportion": ("BOOLEAN", { "default": False }),
+                "keep_proportion": ("BOOLEAN", {"default": False}),
             }
         }
 
@@ -57,17 +65,28 @@ class ImageResize:
             width = ow if width == 0 else width
             height = oh if height == 0 else height
             ratio = min(width / ow, height / oh)
-            width = round(ow*ratio)
-            height = round(oh*ratio)
+            width = round(ow * ratio)
+            height = round(oh * ratio)
 
         width = width // 8 * 8
         height = height // 8 * 8
-        
+
         outputs = p(image)
         if interpolation == "lanczos":
             outputs = comfy.utils.lanczos(outputs, width, height)
         else:
-            outputs = F.interpolate(outputs, size=(height, width), mode=interpolation)
+            outputs = F.interpolate(outputs, size=(
+                height, width), mode=interpolation)
         outputs = pb(outputs)
 
-        return(outputs, outputs.shape[2], outputs.shape[1],)
+        return (outputs, outputs.shape[2], outputs.shape[1],)
+
+
+NODE_CLASS_MAPPINGS = {
+    "ImageResize+": ImageResize,
+}
+
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ImageResize+": "ImageResize+",
+}
