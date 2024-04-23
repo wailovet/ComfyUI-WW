c6a27977588acbb1577331fca823bb0520b4c713
wailovet <wailovet@163.com>
2024-04-24 00:17:02 +0800

    'update'

--- __init__.py
+++ __init__.py
@@ -344,3 +344,78 @@ def clean_all_preview(request):
     PRE_PREVIEW_IMAGE = []
     ALL_PREVIEW_IMAGE = []
     return web.json_response({'data': 'ok'})
+
+
+
+
+
+class ColorMatch:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image_ref": ("IMAGE",),
+                "image_target": ("IMAGE",),
+                "method": (
+            [   
+                'mkl',
+                'hm', 
+                'reinhard', 
+                'mvgd', 
+                'hm-mvgd-hm', 
+                'hm-mkl-hm',
+            ], {
+               "default": 'mkl'
+            }),
+                
+            },
+        }
+    
+    CATEGORY = "KJNodes/image"
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "colormatch"
+    DESCRIPTION = """
+color-matcher enables color transfer across images which comes in handy for automatic  
+color-grading of photographs, paintings and film sequences as well as light-field  
+and stopmotion corrections.  
+
+The methods behind the mappings are based on the approach from Reinhard et al.,  
+the Monge-Kantorovich Linearization (MKL) as proposed by Pitie et al. and our analytical solution  
+to a Multi-Variate Gaussian Distribution (MVGD) transfer in conjunction with classical histogram   
+matching. As shown below our HM-MVGD-HM compound outperforms existing methods.   
+https://github.com/hahnec/color-matcher/
+
+"""
+    
+    def colormatch(self, image_ref, image_target, method):
+        try:
+            from color_matcher import ColorMatcher
+        except:
+            raise Exception("Can't import color-matcher, did you install requirements.txt? Manual install: pip install color-matcher")
+        cm = ColorMatcher()
+        image_ref = image_ref.cpu()
+        image_target = image_target.cpu()
+        batch_size = image_target.size(0)
+        out = []
+        images_target = image_target.squeeze()
+        images_ref = image_ref.squeeze()
+
+        image_ref_np = images_ref.numpy()
+        images_target_np = images_target.numpy()
+
+        if image_ref.size(0) > 1 and image_ref.size(0) != batch_size:
+            raise ValueError("ColorMatch: Use either single reference image or a matching batch of reference images.")
+
+        for i in range(batch_size):
+            image_target_np = images_target_np if batch_size == 1 else images_target[i].numpy()
+            image_ref_np_i = image_ref_np if image_ref.size(0) == 1 else images_ref[i].numpy()
+            try:
+                image_result = cm.transfer(src=image_target_np, ref=image_ref_np_i, method=method)
+            except BaseException as e:
+                print(f"Error occurred during transfer: {e}")
+                break
+            out.append(torch.from_numpy(image_result))
+        return (torch.stack(out, dim=0).to(torch.float32), )
+    
